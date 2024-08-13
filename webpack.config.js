0a2d79a4389b3cda6280c28f4690c613b1682e8e
rsninja722 <jnick722547@gmail.com>
2024-08-13 18:52:13 -0400

    Rename dist folder for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'production',
     entry: path.resolve(__dirname, 'src/index.js'),
     output: { 
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js',
     },
     module: {
